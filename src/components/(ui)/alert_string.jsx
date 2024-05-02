ed60e170ad194c41657948f208efd8ed7477cee9
phamtiendat01 <phamtiendat.112004@gmail.com>
2024-05-03 00:56:14 +0700

    Change some textstexts

--- src/components/(ui)/alert_string.jsx
+++ src/components/(ui)/alert_string.jsx
@@ -4,8 +4,8 @@ function AlertString({temp}) {
   console.log(temp)
   return (
     <div className='font-bold text-xl'>
-        {temp > 40 && <p className='text-red-500'>Cháy!!!</p>}
-        {temp > 30 && <p className='text-yellow-500'>Cảnh báo!!!</p>}
+        {temp > 40 && <p className='text-red-500'>Phát hiện cháy !!!</p>}
+        {temp > 30 && <p className='text-yellow-500'>Cảnh báo cháy nổ !!!</p>}
         {temp <= 30 && <p className='text-emerald-500'>Bình thường</p>}
         {temp <= 10 && <p className='text-sky-500'>Lạnh!!!</p>}
         {/* {!temp && <p className='text-red-500'>Không có thông tin!!!</p>} */}
